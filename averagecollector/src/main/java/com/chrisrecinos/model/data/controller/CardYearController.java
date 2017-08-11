af8e14884b7092fb3d1ac3a028e2ddd031349be4
Christopher Recinos <chrisrecinos38@gmail.com>
2017-08-11 09:18:29 -0400

    Modified build.gradle and CardYearController to work with Thymeleaf

--- averagecollector/src/main/java/com/chrisrecinos/model/data/controller/CardYearController.java
+++ averagecollector/src/main/java/com/chrisrecinos/model/data/controller/CardYearController.java
@@ -4,24 +4,30 @@ import com.chrisrecinos.model.data.entity.CardYear;
 
 import com.chrisrecinos.model.data.services.CardYearService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 /**
  * @author - Christopher Recinos
  */
 
-@RestController
+@Controller
+@RequestMapping(value = "/years")
 public class CardYearController {
 
     @Autowired
     private CardYearService cardYearService;
 
-    @RequestMapping(value = "/years", method = RequestMethod.GET)
-    public List<CardYear> getYears(@RequestParam(required = false) Integer year) {
-        return this.cardYearService.getYearsWithYearName(year);
+    @RequestMapping(method = RequestMethod.GET)
+    public String getYears(@RequestParam(required = false) Integer year,
+                                   Model model) {
+        List<CardYear> cardYears = this.cardYearService.getYearsWithYearName(year);
+        model.addAttribute("cardYears", cardYears);
+
+        return "years";
     }
 }
