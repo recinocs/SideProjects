42b16a5676805aa13437976eec38187b82fca621
Christopher Recinos <recinocs@dukes.jmu.edu>
2017-08-28 21:00:25 -0400

    Sets now sorted
    Sets are being sorted by cardYear

--- averagecollector/src/main/java/com/chrisrecinos/model/data/controller/CardSetController.java
+++ averagecollector/src/main/java/com/chrisrecinos/model/data/controller/CardSetController.java
@@ -1,6 +1,7 @@
 package com.chrisrecinos.model.data.controller;
 
 import com.chrisrecinos.model.data.entity.CardSet;
+import com.chrisrecinos.model.data.entity.CardYear;
 import com.chrisrecinos.model.data.services.CardSetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author - Christopher Recinos
@@ -28,8 +29,34 @@ public class CardSetController {
                           @RequestParam(required = false) String setName,
                           Model model) {
         List<CardSet> cardSetList = this.cardSetService.getSets(year, brandName, setName);
-        model.addAttribute("sets", cardSetList);
+
+        model.addAttribute("sets", sort(cardSetList));
 
         return "sets";
+    }
+
+    private List<CardSet> sort(List<CardSet> sets) {
+        Map<Integer, List<CardSet>> yearsToSets = new HashMap<>();
+
+        for(CardSet c : sets) {
+            Integer c_year = c.getCardYear().getCardYear();
+            if(yearsToSets.get(c_year) == null) {
+                yearsToSets.put(c_year, new ArrayList<>());
+                yearsToSets.get(c_year).add(c);
+            }
+        }
+
+        List<Integer> sortedKeys = new ArrayList<>(yearsToSets.keySet());
+        Collections.sort(sortedKeys);
+
+        List<CardSet> sortedSets = new ArrayList<>();
+
+        for(Integer key : sortedKeys) {
+            List<CardSet> temp = yearsToSets.get(key);
+            for(CardSet set : temp)
+                sortedSets.add(set);
+        }
+
+        return sortedSets;
     }
 }
