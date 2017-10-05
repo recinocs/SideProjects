e07ee4732f1995c05aeb7eaeb07058119f62f857
Christopher Recinos <recinocs@dukes.jmu.edu>
2017-10-05 00:20:54 -0400

    Added homepage
    Updated start page look
    Added in homepage
    Updated StartController for homepage navigation

--- averagecollector/src/main/java/com/chrisrecinos/model/data/controller/StartController.java
+++ averagecollector/src/main/java/com/chrisrecinos/model/data/controller/StartController.java
@@ -1,8 +1,15 @@
 package com.chrisrecinos.model.data.controller;
 
+import com.chrisrecinos.model.data.entity.Team;
+import com.chrisrecinos.model.data.repository.TeamRepository;
+import com.chrisrecinos.model.data.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * @author - Christopher Recinos
@@ -12,8 +19,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/")
 public class StartController {
 
+    @Autowired
+    private TeamService teamService;
+
     @RequestMapping(method = RequestMethod.GET)
-    String getStartPage() {
+    String getIntroPage() {
         return "index";
+    }
+
+    @RequestMapping(value = "/homepage", method = RequestMethod.GET)
+    String navToHomePage(Model model) {
+        model.addAttribute("teams", getAllTeams());
+        return "homepage";
+    }
+
+    private List<Team> getAllTeams() {
+        return this.teamService.getTeams("", "");
     }
 }
