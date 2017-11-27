67a2e5f882144db91025d0edcf9e191bc3d366f8
Christopher Recinos <recinocs@dukes.jmu.edu>
2017-11-26 23:49:23 -0500

    More set and cardnum cucumber tests

--- averagecollector/src/test/java/com/chrisrecinos/cucumber/steps/RunSearchUsingSetAndNumAndInsertSteps.java
+++ averagecollector/src/test/java/com/chrisrecinos/cucumber/steps/RunSearchUsingSetAndNumAndInsertSteps.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 /**
  * @author - Christopher Recinos
  */
-public class RunSearchWithSetAndNumAndInsertAndPlayerSteps extends StepDefinition {
+public class RunSearchUsingSetAndNumAndInsertSteps extends StepDefinition {
 
     @And("^User selects \"(.*)\" as their specified set$")
     public void user_selects_specified_set(String set) throws Throwable {
@@ -18,12 +18,14 @@ public class RunSearchWithSetAndNumAndInsertAndPlayerSteps extends StepDefinitio
 
     @And("^User enters \"(.*)\" as their specified card number$")
     public void user_enters_specified_card_number(String cardNum) throws Throwable {
+        driver.findElement(By.id("card_num")).clear();
         driver.findElement(By.id("card_num")).sendKeys(cardNum);
         Thread.sleep(500);
     }
 
     @And("^User enters \"(.*)\" as their specified insert$")
     public void user_enters_specified_insert(String insert) throws Throwable {
+        driver.findElement(By.id("insert_type")).clear();
         driver.findElement(By.id("insert_type")).sendKeys(insert);
         Thread.sleep(500);
     }
